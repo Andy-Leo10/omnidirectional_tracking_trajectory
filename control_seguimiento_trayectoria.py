53c62e3186084fa0ca4434cbd3fa24a185a39082
Andy-Leo10 <andres.alamo@pucp.edu.pe>
2023-09-23 03:16:56 +0000

    el robot se mueve pero hay bugs peligrosos

--- control_seguimiento_trayectoria.py
+++ control_seguimiento_trayectoria.py
@@ -1,54 +1,7 @@
 from robot_omni_class import *
 import time
 
-def seguimiento_de_trayectoria(xd,xdp,yd,ydp,phi,phid,x_c,y_c):
-    # x_c: es x del centro entre ruedas
-    # y_c: es y del centro entre ruedas
-
-    d2 = 0.500 / 2
-    K1 = 2
-    K2 = 2
-    K3 = 2
-
-    # xdes: es x desplazado
-    # ydes: es y desplazado
-
-    xdesf = x_c + d2*np.cos(phi)
-    ydesf = y_c + d2*np.sin(phi)
-
-    # xd: es x deseado
-    # yd: es y deseado
-
-    xe = xd - xdesf
-    ye = yd - ydesf
-    phie=0
-
-    p1 = K1*xe - xdp
-    p2 = K2*ye - ydp
-    p3 = K3*phie-phid
-
-    av = np.array([xd+p1, yd+p1, phid+p3])
-    av.shape = (3,1)
-
-    # Matriz inversa:
-    # [cos(phi)     sin(phi)    0
-    #  -sin(phi)    cos(phi)    -d2
-    #  0            0           1]
-
-    M= np.array([
-                [np.cos(phi), -np.sin(phi), -d2*np.sin(phi)],
-                [-np.sin(phi), np.cos(phi), d2*np.cos(phi)],
-                [0,             0,              1]
-                ])
-
-    results=np.dot(np.linalg.inv(M),av)
-    
-    u = results[0, 0]
-    v = results[1, 0]
-    w = results[2, 0]
-    return u,v,w
-
-def seguimiento_de_trayectoria2(xd,xdp,yd,ydp,phi,phid,robot):
+def seguimiento_de_trayectoria(xd,xdp,yd,ydp,phi,phid,robot):
     
     position=robot.get_position()
     x_c=position.x
@@ -104,10 +57,11 @@ if __name__ == '__main__':
     try:
         robot = RobotOmni()
         #hacemos una trayectoria de linea recta en el tiempo de 10 segundos que se actualiza cada 0.5 segundos
-        tiempo=0
+        tiempo=0.0
         v=0.5
-        while tiempo<10:
-            tiempo+=0.5
+        dt=0.005
+        while tiempo<3.0:
+            tiempo+=dt
             xd=0
             xdp=0
             yd=v*tiempo
@@ -115,9 +69,12 @@ if __name__ == '__main__':
             phi=0
             phid=0
             #obtenemos las velocidades respecto del robot para seguir la trayectoria
-            u,v,w=seguimiento_de_trayectoria2(xd,xdp,yd,ydp,phi,phid,robot)
+            u,v,w=seguimiento_de_trayectoria(xd,xdp,yd,ydp,phi,phid,robot)
             robot.move(u,v,w)
-            time.sleep(0.5)
+            print(tiempo)
+            time.sleep(dt)
+        robot.stop()
+        print("---FIN---")
     except rospy.ROSInterruptException:
         pass
 
