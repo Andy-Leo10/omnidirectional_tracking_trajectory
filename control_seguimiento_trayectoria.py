c8b131db823c8ee9689c302082c70150ce00e407
Andy-Leo10 <andres.alamo@pucp.edu.pe>
2023-09-29 03:19:39 +0000

    OFICIAL nuevo-olvide actualizar esa variable

--- control_seguimiento_trayectoria.py
+++ control_seguimiento_trayectoria.py
@@ -1,13 +1,12 @@
 from robot_omni_class import *
 import time
 
-def seguimiento_de_trayectoria(xd,xdp,yd,ydp,phid,phidp,robot):
-    
-    position=robot.get_position()
-    x_c=position.x
-    y_c=position.y
-    phidesf=robot.get_yaw()
-    
+def seguimiento_de_trayectoria(xd, xdp, yd, ydp, phid, phidp, robot):
+    position = robot.get_position()
+    x_c = position.x
+    y_c = position.y
+    phidesf = robot.get_yaw()
+
     # x_c: es x del centro entre ruedas
     # y_c: es y del centro entre ruedas
 
@@ -16,43 +15,44 @@ def seguimiento_de_trayectoria(xd,xdp,yd,ydp,phid,phidp,robot):
     K2 = 2
     K3 = 2
 
-    # xdes: es x desplazado
-    # ydes: es y desplazado
+    # xdesf: es x desplazado
+    # ydesf: es y desplazado
 
-    xdesf = x_c + d2*np.cos(phi)
-    ydesf = y_c + d2*np.sin(phi)
+    xdesf = x_c + d2 * np.cos(phidesf)
+    ydesf = y_c + d2 * np.sin(phidesf)
 
     # xd: es x deseado
     # yd: es y deseado
 
     xe = xd - xdesf
     ye = yd - ydesf
-    phie=phid-phidesf
+    phie = phid - phidesf
 
-    p1 = K1*xe - xdp
-    p2 = K2*ye - ydp
-    p3 = K3*phie-phidp
+    p1 = K1 * xe - xdp
+    p2 = K2 * ye - ydp
+    p3 = K3 * phie - phidp
 
     av = np.array([p1, p2, p3])
-    av.shape = (3,1)
+    av.shape = (3, 1)
 
     # Matriz inversa:
     # [cos(phi)     sin(phi)    0
     #  -sin(phi)    cos(phi)    -d2
     #  0            0           1]
 
-    M= np.array([
-                [np.cos(phi), -np.sin(phi), -d2*np.sin(phi)],
-                [-np.sin(phi), np.cos(phi), d2*np.cos(phi)],
-                [0,             0,              1]
-                ])
+    M = np.array([
+        [np.cos(phidesf), -np.sin(phidesf), -d2 * np.sin(phidesf)],
+        [-np.sin(phidesf), np.cos(phidesf), d2 * np.cos(phidesf)],
+        [0, 0, 1]
+    ])
 
-    results=np.dot(np.linalg.inv(M),av)
-    
+    results = np.dot(np.linalg.inv(M), av)
+
     u = results[0, 0]
     v = results[1, 0]
     w = results[2, 0]
-    return u,v,w
+    return u, v, w
+
 
 if __name__ == '__main__':
     try:
