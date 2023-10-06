7f30c86fb8f698d25df13103b95e2486cc95d2f8
Andy-Leo10 <andres.alamo@pucp.edu.pe>
2023-10-06 12:23:13 +0000

    OFICIAL ROBOT CONTROL - tested

--- control_seguimiento_trayectoria.py
+++ control_seguimiento_trayectoria.py
@@ -28,9 +28,9 @@ def seguimiento_de_trayectoria(xd, xdp, yd, ydp, phid, phidp, robot):
     ye = yd - ydesf
     phie = phid - phidesf
 
-    p1 = K1 * xe - xdp
-    p2 = K2 * ye - ydp
-    p3 = K3 * phie - phidp
+    p1 = K1 * xe + xdp
+    p2 = K2 * ye + ydp
+    p3 = K3 * phie + phidp
 
     av = np.array([p1, p2, p3])
     av.shape = (3, 1)
@@ -53,48 +53,48 @@ def seguimiento_de_trayectoria(xd, xdp, yd, ydp, phid, phidp, robot):
     w = results[2, 0]
     return u, v, w
 
+def seguir_trayectoria(robot, trayectoria):
+    tiempo = 0.0
+    dt = 0.005
+
+    for punto in trayectoria:
+        xd, xdp, yd, ydp, phi, phid, duracion = punto
+        while tiempo < duracion:  # Controla el tiempo en cada segmento de la trayectoria
+            # Obtener velocidades para seguir el punto actual
+            u, v, w = seguimiento_de_trayectoria(xd, xdp, yd, ydp, phi, phid, robot)
+            # Mover el robot y actualizar el tiempo
+            robot.move(u, v, w)
+            tiempo += dt
+            time.sleep(dt)
+        tiempo=0
+        # Detener el robot al final de cada segmento
+        robot.stop()
+        print("---FIN DEL SEGMENTO---")
+
+    print("---FIN DE LA TRAYECTORIA---")
 
 if __name__ == '__main__':
     try:
         robot = RobotOmni()
-        #hacemos una trayectoria que se actualiza cada 0.5 segundos
-        tiempo=0.0
-        v=0.5
-        dt=0.005
-        while tiempo<3.0:
-            xd=2
-            xdp=0
-            yd=2
-            ydp=0
-            phi=pi/4
-            phid=0
-            #obtenemos las velocidades respecto del robot para seguir la trayectoria
-            u,v,w=seguimiento_de_trayectoria(xd,xdp,yd,ydp,phi,phid,robot)
-            robot.move(u,v,w)
-            print(tiempo)
-            tiempo+=dt
-            time.sleep(dt)
-        robot.stop()
-        tiempo=0
-        while tiempo<3.0:
-            xd=-1
-            xdp=0
-            yd=-3
-            ydp=0
-            phi=-pi/2
-            phid=0
-            #obtenemos las velocidades respecto del robot para seguir la trayectoria
-            u,v,w=seguimiento_de_trayectoria(xd,xdp,yd,ydp,phi,phid,robot)
-            robot.move(u,v,w)
-            print(tiempo)
-            tiempo+=dt
-            time.sleep(dt)
-        robot.stop()
+
+        # Definir los puntos de la trayectoria (llegar a la escalera)
+        trayectoria = [
+            #(1.0, 0.0, 1.0, 0.0, 0.0, 0.0, t),  # Ejemplo: Avanzar 1 metro en diagonal
+            # (xd, xdp, yd, ydp, phi, phid,t)
+            # Define los puntos de la trayectoria aqui
+            (0.0, 0.0, 7.0, 0.0, pi/2, 0.0,6),
+            (-0.7, 0.0, 7.2, 0.0, 3*pi/2, 0.0,1),
+            (-1.2, 0.0, 7.7, 0.0, pi, 0.0,2)
+        ]
+
+        seguir_trayectoria(robot, trayectoria)
         print("---FIN---")
+
     except rospy.ROSInterruptException:
         pass
 
 
+#cd /home/user/catkin_ws/src/omni_pkg/src
 #roslaunch neo_description mpo_500_controller.launch
-#cd ~/catkin_ws/src/omni_pkg/src
 #python control_seguimiento_trayectoria.py
+#roslaunch neo_description mpo_500_controller.launch world_name_global:=/home/user/catkin_ws/src/omni_pkg/src/miDepaWorld.world
